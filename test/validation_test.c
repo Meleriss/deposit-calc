b24071a429ff564cd999ccc64bd583ce2b8f5010
Березина Мария Юрьевна <iv621s02@awp01.csc.local>
2017-04-22 12:52:43 +0700

    Validation test

--- test/validation_test.c
+++ test/validation_test.c
@@ -11,3 +11,36 @@ CTEST(vapod_test, wrong_aod_pod)
     const int expected = -1;
     ASSERT_EQUAL(expected, result);
 }
+
+CTEST(vapod_test, wrong_aod)
+{
+	const int aod = 2;
+    const int pod = 365;
+
+    const int result = vapod(aod, pod);
+
+    const int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(vapod_test, wrong_pod)
+{
+	const int aod = 15000;
+    const int pod = 367;
+
+    const int result = vapod(aod, pod);
+
+    const int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(vapod_test, wrong_no)
+{
+	const int aod = 20000;
+    const int pod = 1;
+
+    const int result = vapod(aod, pod);
+
+    const int expected = 0;
+    ASSERT_EQUAL(expected, result);
+}
